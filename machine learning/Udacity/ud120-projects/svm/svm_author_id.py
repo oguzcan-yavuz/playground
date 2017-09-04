14b92bc72c65c004ccf1cc014e48fcdf4bcc2419
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-09-05 00:08:41 +0300

    svm mini project completed.

--- machine learning/Udacity/ud120-projects/svm/svm_author_id.py
+++ machine learning/Udacity/ud120-projects/svm/svm_author_id.py
@@ -9,6 +9,7 @@
 """
     
 import sys
+import collections
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
@@ -20,18 +21,25 @@ from sklearn.metrics import accuracy_score
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-features_train = features_train[:len(features_train) / 100]
-labels_train = labels_train[:len(labels_train) / 100]
+
+# use %1 of the data for more speed. accuracy drops to 0.88 while it is 0.98 with full data
+# without c optimization. with c optimization and full data our accuracy is = 0.990898748578
+# with c opt. and %1 data our accuracy is = 0.892491467577
+# features_train = features_train[:len(features_train) / 100]
+# labels_train = labels_train[:len(labels_train) / 100]
 
 #########################################################
-clf = svm.SVC(kernel='linear')
+
+clf = svm.SVC(kernel='rbf', C=10000.0)
 t0 = time()
 clf.fit(features_train, labels_train)
 print("training time: {0} s".format(round(time() - t0, 3)))
+# predicts = [features_test[10], features_test[26], features_test[50]]
+# pred = clf.predict(predicts)
 pred = clf.predict(features_test)
-
-print(accuracy_score(pred, labels_test))
+print(sum(pred))    # we are printing the sum of a list with values 1 or 0 to find how many 1 it contains
+# pred = clf.predict(features_test)
+# print("accuracy: {0}".format(accuracy_score(pred, labels_test)))
 
 #########################################################
-
 
