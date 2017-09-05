4c3798ec71a2514317679878f2944228e62ea9cd
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-09-06 02:02:48 +0300

    Outliers detected by their large error rate and %10 of the data is deleted due to their large error rates.

--- machine learning/Udacity/ud120-projects/outliers/outlier_cleaner.py
+++ machine learning/Udacity/ud120-projects/outliers/outlier_cleaner.py
@@ -14,7 +14,12 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    # create cleaned_data as in asked format.
+    for i in range(len(predictions)):
+        cleaned_data.append((ages[i], net_worths[i], float(net_worths[i] - predictions[i])))
 
-    
+    # sort the data by absolute value of the error rate
+    cleaned_data = sorted(cleaned_data, key=lambda n: abs(n[2]))
+    # delete last 10% elements of the data which have the largest error rate
+    cleaned_data = cleaned_data[:-len(ages) / 10]
     return cleaned_data
-
