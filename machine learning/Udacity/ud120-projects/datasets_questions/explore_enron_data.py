bc75332690f9529aa7a0f1447bbb16277bf9a384
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-09-05 20:22:32 +0300

    enron dataset inspected.

--- machine learning/Udacity/ud120-projects/datasets_questions/explore_enron_data.py
+++ machine learning/Udacity/ud120-projects/datasets_questions/explore_enron_data.py
@@ -19,4 +19,31 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+# count = 0
+# for v in enron_data:
+#     if enron_data[v]['poi'] == 1:
+#         count += 1
+# print(count)
 
+# print(enron_data['PRENTICE JAMES']['total_stock_value'])
+
+# print(enron_data["SKILLING JEFFREY K"]["total_payments"])
+# print(enron_data["FASTOW ANDREW S"]["total_payments"])
+# print(enron_data["LAY KENNETH L"]["total_payments"])
+# {'salary': 1072321, 'to_messages': 4273, 'deferral_payments': 202911, 'total_payments': 103559793, 'exercised_stock_options': 34348384, 'bonus': 7000000, 'restricted_stock': 14761694, 'shared_receipt_with_poi': 2411, 'restricted_stock_deferred': 'NaN', 'total_stock_value': 49110078, 'expenses': 99832, 'loan_advances': 81525000, 'from_messages': 36, 'other': 10359729, 'from_this_person_to_poi': 16, 'poi': True, 'director_fees': 'NaN', 'deferred_income': -300000, 'long_term_incentive': 3600000, 'email_address': 'kenneth.lay@enron.com', 'from_poi_to_this_person': 123}
+
+counts = [0, 0, 0, [0, 0]]
+for v in enron_data:
+    if enron_data[v]['salary'] != "NaN":
+        counts[0] += 1
+    if enron_data[v]['email_address'] != "NaN":
+        counts[1] += 1
+    if enron_data[v]['total_payments'] == "NaN":
+        counts[2] += 1
+    if enron_data[v]['poi']:
+        counts[3][0] += 1
+        if enron_data[v]['total_payments'] == "NaN":
+            counts[3][1] += 1
+print(counts)
+print(counts[2] * 100.0 / len(enron_data))
+print(counts[3][1] * 100.0 / counts[3][0])
