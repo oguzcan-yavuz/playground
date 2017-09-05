5a1213879aa7549c7eae0fe9fa74ff777ad05c6f
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-09-06 00:29:25 +0300

    LinearRegression mini project completed.

--- machine learning/Udacity/ud120-projects/regression/finance_regression.py
+++ machine learning/Udacity/ud120-projects/regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,12 +37,13 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print(reg.coef_)
+print(reg.intercept_)
+# print(reg.score(feature_train, target_train))
+print(reg.score(feature_test, target_test))
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +65,14 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print("new line slope")
+print(reg.coef_)
+print("new line intercept")
+print(reg.intercept_)
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
