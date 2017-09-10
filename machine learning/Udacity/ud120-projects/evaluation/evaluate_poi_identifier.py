1b637074f41d4a00b8795d3fc1ac9a885a9d65c6
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-09-10 12:34:15 +0300

    Confusion matrix of our DecisionTree from validate_poi is inspected. Precision score and recall score are inspected.

--- machine learning/Udacity/ud120-projects/evaluation/evaluate_poi_identifier.py
+++ machine learning/Udacity/ud120-projects/evaluation/evaluate_poi_identifier.py
@@ -25,7 +25,28 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+### your code goes here
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_score, recall_score
+features_training, features_test, labels_training, labels_test = train_test_split(features, labels, random_state=42, test_size=0.3)
+print(labels_test.count(1.0))       # gives the number of POI in our test set
+print(len(features_test))           # gives the number of people in our test set
+# initialize the classifier
+clf = DecisionTreeClassifier()
+# fits the data to our classifier
+clf.fit(features_training, labels_training)
+# prints the accuracy
+print(clf.score(features_test, labels_test))
+pred = clf.predict(features_test)
 
-### your code goes here 
+# checks for true positives
+for actual, predict in zip(labels_test, pred):
+    if actual == 1.0 and predict == 1.0:
+        print(actual, predict)
 
+
+# calculates the precision and recall scores
+print(precision_score(labels_test, pred))
+print(recall_score(labels_test, pred))
 
