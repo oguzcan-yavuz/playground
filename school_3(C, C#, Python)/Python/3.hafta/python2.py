357f0d7c96ddaab33bde44a1485ad9236aeaaf2e
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-10-03 21:34:04 +0300

    some modifications on my notes.

--- school_3(C, C#, Python)/Python/3.hafta/python2.py
+++ school_3(C, C#, Python)/Python/3.hafta/python2.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 # -*- coding: cp1254 -*-
 
-# a = raw_input("a\n")
-# b = raw_input("b\n")
-# print(a + b)
-# x = input("x\n")
-# y = input("y\n")
-# print("%d" % (x + y))
+a = raw_input("a\n")
+b = raw_input("b\n")
+print(a + b)
+x = input("x\n")
+y = input("y\n")
+print("%d" % (x + y))
 
 my_list = [1, 2, 3]
 print("%s" % my_list)
 
 print(my_list * 3)      # [1, 2, 3, 1, 2, 3, 1, 2, 3]
 
-# t = str(input("lol\n"))
-# print(t)        # python 2'de calismaz. string almak icin raw_input() kullanmak lazim
+t = str(input("lol\n"))
+print(t)        # python 2'de calismaz. string almak icin raw_input() kullanmak lazim
 
 print(my_list is [1, 2, 3])     # False
 
@@ -38,7 +38,3 @@ if mod == 0 or mod == 2:
     print mod
 else:
     print "yanlis mod degeri"
-
-
-
-
