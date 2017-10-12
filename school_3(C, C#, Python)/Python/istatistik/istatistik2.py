e9afa431c7503f16b66c9656ee37ff29712a4fa6
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-10-12 22:49:23 +0300

    I changed my mind about giving parameters to the class constructor. Now I want separate data parameter in every function. By doing it like this, user can use every function with different datas without creating new instances.

--- school_3(C, C#, Python)/Python/istatistik/istatistik2.py
+++ school_3(C, C#, Python)/Python/istatistik/istatistik2.py
@@ -2,61 +2,79 @@ from math import floor
 
 
 class Seri:
-    def __init__(self, data, grup_sayisi):
-        self.data = sorted(data)
-        self.grup_sayisi = grup_sayisi
-        self.toplam_frekans = len(data)
+    # TODO: yorumlari docstring'e(fonksiyon icerisinde """uctirnak""" icerisine yazilan yorumlar) cevir.
+    # TODO: butun fonksiyonlari tekrar dene ve duzgunce dokumantasyonunu yap, unutuyorsun.
+    def __init__(self):
+        pass
 
-    def basit_seri(self):
-        return self.data
+    def basit_seri(self, data):
+        return sorted(data)
 
-    def frekans_serisi(self, gruplar=None):
-        if gruplar is None:
-            return [[i, self.data.count(i)] for i in self.data]
+    # params:
+    # gruplar: eger data gruplandirilmis_seri datasi ise gruplar parametresini
+    # True yap, eger False(default) ise tek boyutlu bir dizi icin calisir.
+    def frekans_serisi(self, data, gruplar=False):
+        # TODO: bu fonksiyon set dondursun.
+        if not gruplar:
+            return [[i, data.count(i)] for i in data]
         else:
-            return [[[g0, g1], sum([self.data.count(i) for i in range(g0, g1)])] for g0, g1 in gruplar]
+            return [[[g0, g1], sum([data.count(i) for i in range(g0, g1)])] for g0, g1 in data]
 
-    def gruplandirilmis_seri(self, retry=0):
-        maximum, minimum = self.data[-1], self.data[0]
+    # params:
+    # grup_sayisi: verinin kac gruba ayrilacagini belirler
+    # retry = genislik hesabinda yukari yuvarlama basarisiz olursa bir fazlasini sadece
+    # bir kere denemek icin fonksiyonun kendi icerisinde recursive olarak kullandigi bir parametre.
+    def gruplandirilmis_seri(self, data, grup_sayisi, retry=0):
+        maximum, minimum = data[-1], data[0]
         aciklik = maximum - minimum
-        grup_genisligi = floor(aciklik / self.grup_sayisi + 1) + retry
-        last_group_start = self.data[0] + (grup_genisligi * (self.grup_sayisi - 1))
-        if maximum < last_group_start or maximum >= last_group_start + grup_genisligi:
-            return self.gruplandirilmis_seri(retry=1) if retry == 0 else "Gruplandirma yapilamiyor."
-        return [
-            [
-                self.data[0] + (i * grup_genisligi) + i,
-                self.data[0] + ((i + 1) * grup_genisligi) + i
-            ] for i in range(self.grup_sayisi)
-        ]
+        genislik = floor(aciklik / grup_sayisi + 1) + retry
+        last_group_start = data[0] + (genislik * (grup_sayisi - 1))
+        if maximum < last_group_start or maximum >= last_group_start + genislik:
+            return self.gruplandirilmis_seri(data, grup_sayisi, retry=1) if retry == 0 else "Gruplandirma yapilamiyor."
+        return [[data[0] + (i * genislik) + i, data[0] + ((i + 1) * genislik) + i] for i in range(grup_sayisi)]
 
+    # params:
+    # gruplar: gruplandirilmis_seri fonksiyonundan donen bir deger bekler
     def temsili_datalar(self, gruplar):
         return [(g[0] + g[1]) / 2 for g in gruplar]
 
-    def kismi_frekans(self, frekanslar):
-        return [(f / self.toplam_frekans) for _, f in frekanslar]
+    # params:
+    # frekans_serisi: frekans_serisi(gruplar=False) fonksiyonundan donen bir deger bekler
+    # toplam_frekans: toplam data sayisi.
+    def kismi_frekans(self, frekans_serisi, toplam_frekans):
+        return [(f / toplam_frekans) for _, f in frekans_serisi]
 
-    def kumulatif_frekans(self, frekanslar):
-        return [[sum([frekanslar[j][1] for j in range(i + 1)]) / self.toplam_frekans] for i in range(self.grup_sayisi)]
+    # params:
+    # frekans_serisi: frekans_serisi(gruplar=False) fonksiyonundan donen bir deger bekler
+    # toplam_frekans: toplam data sayisi.
+    def kumulatif_frekans(self, frekans_serisi, toplam_frekans, grup_sayisi):
+        # kf = self.kumulatif_frekans(self.frekans_serisi(data), len(data), grup_sayisi)
+        return [[sum([frekans_serisi[j][1] for j in range(i + 1)]) / toplam_frekans] for i in range(grup_sayisi)]
 
     # TODO: check if this function is for cumilative frequencies or partial frequency
-    def kumulatif_relatif_frekans(self, kismi_frekanslar):
-        return [kf / self.toplam_frekans for kf in kismi_frekanslar]
+    # params:
+    # toplam_frekans: toplam data sayisi.
+    # kismi_frekanslar: kismi_frekans fonksiyonundan donen bir deger bekler
+    def kumulatif_relatif_frekans(self, kismi_frekanslar, toplam_frekans):
+        return [kf / toplam_frekans for kf in kismi_frekanslar]
 
-    def birikimli_azalan(self, frekanslar):
-        return [[sum([frekanslar[j][1] for j in range(i, -1, -1)]) for i in range(self.grup_sayisi - 1, -1, -1)]]
+    # params:
+    # gruplandirilmis_frekanslar: frekans_serisi(gruplar=True) fonk. donus degeri bekler
+    # grup_sayisi: grup sayisini belirler
+    def birikimli_azalan(self, gruplandirilmis_frekanslar, grup_sayisi):
+        return [[sum([gruplandirilmis_frekanslar[j][1] for j in range(i, -1, -1)]) for i in range(grup_sayisi - 1, -1, -1)]]
 
 
 class AritmetikOrtalama(Seri):
-    def basit(self):
-        return sum(self.data) / len(self.data)
+    def basit(self, data):
+        return sum(data) / len(data)
 
-    def frekans(self):
-        frekans_serisi = self.frekans_serisi()
+    def frekans(self, data):
+        frekans_serisi = self.frekans_serisi(data)
         return sum([s * f for s, f in frekans_serisi]) / sum([f for s, f in frekans_serisi])
 
-    def gruplandirilmis(self):
-        gruplandirilmis_seri = self.gruplandirilmis_seri()
+    def gruplandirilmis(self, data, grup_sayisi):
+        gruplandirilmis_seri = self.gruplandirilmis_seri(data, grup_sayisi)
         temsili_data = self.temsili_datalar(gruplandirilmis_seri)
         frekans_serisi = self.frekans_serisi(gruplandirilmis_seri)
         result = 0
@@ -68,9 +86,12 @@ class AritmetikOrtalama(Seri):
 
 
 notlar = [24, 80, 52, 65, 40, 40, 65, 50, 36, 60, 75, 40, 60, 95, 50, 30, 52, 24, 40, 75]
-aritmetik = AritmetikOrtalama(notlar, 5)
+aritmetik = AritmetikOrtalama()
 
-print("basit seri: {0}".format(aritmetik.basit_seri()))
-print("frekans serisi: {0}".format(aritmetik.frekans_serisi()))
-print("gruplandirilmis seri: {0}".format(aritmetik.gruplandirilmis_seri()))
-print(aritmetik.gruplandirilmis())
+# print("basit seri: {0}".format(aritmetik.basit_seri()))
+# print("frekans serisi: {0}".format(aritmetik.frekans_serisi()))
+# print("gruplandirilmis seri: {0}".format(aritmetik.gruplandirilmis_seri()))
+# print(aritmetik.gruplandirilmis())
+print(aritmetik.gruplandirilmis_seri(notlar, 5))
+frekans_serisi = aritmetik.frekans_serisi(notlar)
+print(aritmetik.kumulatif_frekans(frekans_serisi, len(frekans_serisi), 6))
