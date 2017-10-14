097a32651a1dd448408cd76167294462608d9085
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-10-14 16:05:04 +0300

    Assembly folder created.

---
+++ assembly/README.md
@@ -0,0 +1,6 @@
+### I followed this tutorial:
+    - https://www.tutorialspoint.com/assembly_programming
+
+### Example usages of nasm and ld
+    - nasm -f elf hello.asm
+    - ld -m elf_i386 -s -o hello hello.o
--- python/2000.py
+++ python/2000.py
@@ -1,7 +1,9 @@
-fp = open("random.txt", "r")
+fp = open("random.txt", "r")        # random.txt line example: 1234 5678 1234 5678
 result = 0
 for i, line in enumerate(fp, 1):
+    # if we are at the 2000th line
     if i % 2000 == 0:
-        result += int(line[15:])
+        # last 4 digit number is our result.
+        result = int(line[15:])
 print(result)
 fp.close()
