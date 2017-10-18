0a644ef93c1a0b4b252d55eda4665e839e1ed2f4
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-10-18 20:45:14 +0300

    Structure of istatistik2.py is changed.

--- school_3(C, C#, Python)/Ödev/istatistik/istatistik2.py
+++ school_3(C, C#, Python)/Ödev/istatistik/istatistik2.py
@@ -2,20 +2,30 @@
 from math import floor
 
 
-class Seri:
-    # TODO: butun fonksiyonlari tekrar dene ve duzgunce dokumantasyonunu yap, unutuyorsun.
+# TODO: butun fonksiyonlari tekrar dene ve duzgunce dokumantasyonunu yap, unutuyorsun.
+class BasitSeri(object):
     def __init__(self):
         pass
 
-    def basit_seri(self, data):
-        """Verilen veriyi küçükten büyüğe sıralar.
+    @staticmethod
+    def basit_seri(data):
+        """
+        Verilen veriyi küçükten büyüğe sıralar.
         :param data: Tek boyutlu bir liste.
         :returns: Verilen listenin küçükten büyüğe sıralanmış hali.
         """
         return sorted(data)
 
-    def frekans_serisi(self, data, gruplar=False):
-        """Verilen verinin frekans serisini döndürür.
+
+class FrekansSerisi(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def frekans_serisi(data, gruplar=False):
+        # TODO: returns kismini tamamla
+        """
+        Verilen verinin frekans serisini döndürür.
         :param data: Tek boyutlu bir liste veya gruplandırılmış seri.
         :param gruplar: Eğer data gruplandırılmış seri ise bu parametre True yapılmalıdır.
         :returns: Gruplar parametresi False ise iki boyutlu, her bir elemanı
@@ -23,55 +33,19 @@ class Seri:
             gruplar parametresi True ise...
         """
         if gruplar is False:
-            # TODO: butun listeleri tuple'a donusturmek mantikli mi? yoksa sadece burada
-            # mi tuple kalsin? yoksa uniq fonksiyonu olusturmak mi daha mantikli?
             return set([(i, data.count(i)) for i in data])
-            # return [[i, data.count(i)] for i in data]
         else:
             return [[[g0, g1], sum([data.count(i) for i in range(g0, g1)])] for g0, g1 in data]
 
-    def gruplandirilmis_seri(self, data, grup_sayisi, retry=0):
-        """Verilen verinin istenilen grup sayısında gruplandırılmış halini döndürür.
-        Eğer istenilen sayıda gruplandırma yapılamıyorsa 'Gruplandırılma yapılamıyor!'
-        string'ini döndürür
-        :param data: Tek boyutlu veri.
-        :param grup_sayisi: Verinin bölünmek istendiği grup sayısı
-        :param retry: Fonksiyonun kendi içerisinde genişliği kontrol etmek için kullandığı
-        fonksiyonun kendisini çağırma sayısını tutan default parametre. Dokunmayın.
-        :returns: İstenilen sayıda gruplandırılmış verinin liste halindeki alt ve üst sınırları
-        her bir elemanını oluşturan bir liste.
+    def kismi_frekans(self, frekans_serisi):
         """
-        maximum, minimum = data[-1], data[0]
-        aciklik = maximum - minimum
-        genislik = floor(aciklik / grup_sayisi + 1) + retry
-        son_grup_baslangici = data[0] + (genislik * (grup_sayisi - 1))
-        if maximum < son_grup_baslangici or maximum >= son_grup_baslangici + genislik:
-            return self.gruplandirilmis_seri(data, grup_sayisi, retry=1) if retry == 0 else "Gruplandırma yapılamıyor!"
-        return [[data[0] + (i * genislik) + i, data[0] + ((i + 1) * genislik) + i] for i in range(grup_sayisi)]
-
-    def temsili_datalar(self, gruplar):
-        """Gruplandırılmış serideki her bir grup için temsili değeri hesaplar.
-        :param gruplar: Gruplandırılmış seri.
-        :returns: Her grubun indexine karşılık gelecek şekilde temsili değerlerini tutan tek
-        boyutlu bir dizi.
+        Kısmi frekans, frekans serisindeki frekansların toplam frekansa bölümüdür.
+        :param frekans_serisi: Tek boyutlu frekans serisi.
+        :returns: Her bir index, frekans serisindeki verinin indexine denk gelecek şekilde
+        verilerin kısmi frekanslarını içeren tek boyutlu bir liste.
         """
-        return [(g[0] + g[1]) / 2 for g in gruplar]
-
-    # params:
-    # frekans_serisi: frekans_serisi(gruplar=False) fonksiyonundan donen bir deger bekler
-    # toplam_frekans: toplam data sayisi.
-    def kismi_frekans(self, frekans_serisi, toplam_frekans):
-        # TODO: toplam frekansi arguman olarak vermek mi yoksa fonk. icinde hesaplamak mi?
-        """Kısmi frekans, frekans serisindeki frekansların toplam frekansa bölümüdür.
-        """
+        toplam_frekans = sum([f for _, f in frekans_serisi])
         return [(f / toplam_frekans) for _, f in frekans_serisi]
-
-    # params:
-    # frekans_serisi: frekans_serisi(gruplar=False) fonksiyonundan donen bir deger bekler
-    # toplam_frekans: toplam data sayisi.
-    def kumulatif_frekans(self, frekans_serisi, toplam_frekans, grup_sayisi):
-        # kf = self.kumulatif_frekans(self.frekans_serisi(data), len(data), grup_sayisi)
-        return [[sum([frekans_serisi[j][1] for j in range(i + 1)]) / toplam_frekans] for i in range(grup_sayisi)]
 
     # TODO: check if this function is for cumilative frequencies or partial frequency
     # params:
@@ -80,60 +54,116 @@ class Seri:
     def kumulatif_relatif_frekans(self, kismi_frekanslar, toplam_frekans):
         return [kf / toplam_frekans for kf in kismi_frekanslar]
 
-    # params:
-    # gruplandirilmis_frekanslar: frekans_serisi(gruplar=True) fonk. donus degeri bekler
-    # grup_sayisi: grup sayisini belirler
-    def birikimli_azalan(self, gruplandirilmis_frekanslar, grup_sayisi):
-        return [[sum([gruplandirilmis_frekanslar[j][1] for j in range(i, -1, -1)]) for i in range(grup_sayisi - 1, -1, -1)]]
+
+class GruplandirilmisSeri(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def gruplandirilmis_seri(data, grup_sayisi, _retry=0):
+        """
+        Verilen verinin istenilen grup sayısında gruplandırılmış halini döndürür.
+        Eğer istenilen sayıda gruplandırma yapılamıyorsa 'Gruplandırılma yapılamıyor!'
+        string'ini döndürür
+        :param data: Tek boyutlu veri.
+        :param grup_sayisi: Verinin bölünmek istendiği grup sayısı
+        :param _retry: Fonksiyonun kendi içerisinde genişliği kontrol etmek için kullandığı
+        fonksiyonun kendisini çağırma sayısını tutan default parametre. Dokunmayın.
+        :returns: İstenilen sayıda gruplandırılmış verinin liste halindeki alt ve üst sınırları
+        her bir elemanını oluşturan bir liste.
+        """
+        maximum, minimum = data[-1], data[0]
+        aciklik = maximum - minimum
+        genislik = floor(aciklik / grup_sayisi + 1) + _retry
+        son_grup_baslangici = data[0] + (genislik * (grup_sayisi - 1))
+        if maximum < son_grup_baslangici or maximum >= son_grup_baslangici + genislik:
+            err = "Gruplandırma yapılamıyor!"
+            return GruplandirilmisSeri.gruplandirilmis_seri(data, grup_sayisi, _retry=1) if _retry == 0 else err
+        return [[data[0] + (i * genislik) + i, data[0] + ((i + 1) * genislik) + i] for i in range(grup_sayisi)]
+
+    def temsili_datalar(self, gruplar):
+        """
+        Gruplandırılmış serideki her bir grup için temsili değeri hesaplar.
+        :param gruplar: Gruplandırılmış seri.
+        :returns: Her grubun indexine karşılık gelecek şekilde temsili değerlerini tutan tek
+        boyutlu bir liste.
+        """
+        return [(g[0] + g[1]) / 2 for g in gruplar]
+
+    def kumulatif_frekans(self, grup_frekans, toplam_frekans):
+        """
+
+        :param grup_frekans: Gruplandırılmış frekans serisi.
+        :param toplam_frekans: Verilen seride kaç tane veri olduğu.
+        :return: Her bir index, verilen grupların indexine denk gelecek şekilde grupların
+        kümülatif frekanslarını içeren tek boyutlu bir liste.
+        """
+        return [sum([grup_frekans[j][1] for j in range(i + 1)]) / toplam_frekans for i in range(len(grup_frekans))]
+
+    def birikimli_azalan(self, grup_frekanslar):
+        """
+
+        :param grup_frekanslar: Gruplandırılmış frekans serisi.
+        :returns: Her bir index, verilen grubun indexine denk gelecek şekilde o grubun
+        birikimli azalan değerlerini barındıran tek boyutlu bir liste.
+        """
+        return [sum([grup_frekanslar[j][1] for j in range(i, -1, -1)]) for i in range(len(grup_frekanslar) - 1, -1, -1)]
 
 
-class AritmetikOrtalama(Seri):
+class AritmetikOrtalama(object):
     def basit(self, data):
         return sum(data) / len(data)
 
     def frekans(self, data):
-        frekans_serisi = self.frekans_serisi(data)
-        return sum([s * f for s, f in frekans_serisi]) / sum([f for s, f in frekans_serisi])
+        _frekans_serisi = FrekansSerisi.frekans_serisi(data)
+        return sum([s * f for s, f in frekans_serisi]) / sum([f for s, f in _frekans_serisi])
 
     def gruplandirilmis(self, data, grup_sayisi):
-        gruplandirilmis_seri = self.gruplandirilmis_seri(data, grup_sayisi)
-        temsili_data = self.temsili_datalar(gruplandirilmis_seri)
-        frekans_serisi = self.frekans_serisi(gruplandirilmis_seri)
+        _grup = GruplandirilmisSeri()
+        _gruplandirilmis_seri = _grup.gruplandirilmis_seri(data, grup_sayisi)
+        _temsili_data = _grup.temsili_datalar(_gruplandirilmis_seri)
+        _frekans_serisi = FrekansSerisi.frekans_serisi(_gruplandirilmis_seri)
         result = 0
         # TODO: check if this needs to be zipped with x[1] of gruplandirilmis_seri
-        data = zip(temsili_data, [x[1] for x in gruplandirilmis_seri])
+        data = zip(_temsili_data, [x[1] for x in _gruplandirilmis_seri])
         for i in data:
             result += i[0] * i[1]
-        return result / sum([f for g, f in frekans_serisi])
+        return result / sum([f for g, f in _frekans_serisi])
 
 
-class Mod(Seri):
+class Mod(object):
     def basit_mod_hesapla(self, data):
-        frekanslar = self.frekans_serisi(data)
-        max_frekans = max(frekanslar, key=lambda n: n[1])
+        # TODO: test et.
+        _frekanslar = FrekansSerisi.frekans_serisi(data)
+        max_frekans = max(_frekanslar, key=lambda n: n[1])
         modlar = []
-        for data, frekans in frekanslar:
+        for data, frekans in _frekanslar:
             if frekans == max_frekans:
                 modlar.append(data)
         return modlar if len(data) != len(modlar) else "Tüm verilerin frekansı eşit olduğundan mod yoktur!"
 
     def grup_mod_hesapla(self, data):
-        frekanslar = self.frekans_serisi(data, gruplar=True)
-        return frekanslar
+        # TODO: fonksiyonu tamamla.
+        _frekanslar = FrekansSerisi.frekans_serisi(data, gruplar=True)
+        return _frekanslar
 
 
-class Medyan(Seri):
+class Medyan(object):
+    # TODO: Yap.
     pass
 
 
 notlar = [24, 80, 52, 65, 40, 40, 65, 50, 36, 60, 75, 40, 60, 95, 50, 30, 52, 24, 40, 75]
-aritmetik = AritmetikOrtalama()
 
 # print("basit seri: {0}".format(aritmetik.basit_seri()))
 # print("frekans serisi: {0}".format(aritmetik.frekans_serisi()))
 # print("gruplandirilmis seri: {0}".format(aritmetik.gruplandirilmis_seri()))
 # print(aritmetik.gruplandirilmis())
-print(aritmetik.gruplandirilmis_seri(notlar, 5))
-frekans_serisi = aritmetik.frekans_serisi(notlar)
+grup = GruplandirilmisSeri()
+frekans_serisi = FrekansSerisi.frekans_serisi(notlar)
+grup_seri = grup.gruplandirilmis_seri(notlar, 5)
+km = grup.kumulatif_frekans(grup_seri, len(notlar))
 print(frekans_serisi)
+print(grup_seri)
+print(km)
 # print(aritmetik.kumulatif_frekans(frekans_serisi, len(frekans_serisi), 6))
