e618fa196347fe8c3dabd0b9fac744cbdd836c70
yvz <oguzcanyavuz@hotmail.com>
2018-04-18 08:08:53 +0300

    trying to complete pairwise.

--- javascript/pairwise.js
+++ javascript/pairwise.js
@@ -1,16 +1,39 @@
-function sumIndexes(arr, a, b, arg) {
-  return (a + b === arg) ? arr.indexOf(a) + arr.indexOf(b) : 0;
+function checkSum(arr, a, b, arg) {
+    if(a + b === arg)
+        console.log("a: " + a + " a index: " + arr.indexOf(a) + " b: " + b + " b index: " + arr.indexOf(b) + " indexSum: " + (arr.indexOf(a) + arr.indexOf(b)));
+    // TODO: indexOf fonksiyonuna, ayni elemanlar verilince aradaki ayrimi dogal olarak yapmiyor, belli bir
+    // indexten sonrasina baktirmak lazim, indexOf ikinci parametre olarak start indexi aliyor. start indexi belirle.
+    // TODO: ayrica eslesen bir ciftin iki elemani da tekrar kullanilamiyor.
+
+    // gen yerine iterator yap. dizinin kopyasini olusturmamak icin. ayrica entries kullan. pair olan elemanlari NaN yap.
+    return (a + b === arg) ? sumOfIndexes(arr, a, b) : 0;
+}
+
+function sumOfIndexes(arr, a, b) {
+    return arr.indexOf(a) + arr.indexOf(b);
+}
+
+function* reduceFromHead(arr) {
+    let i = 0;
+    while(i < arr.length)
+        yield arr.slice(i++);
+}
+
+function checkArrValues(arr, reducedArr, arg) {
+  let sumOfReducedArrIndexes = 0;
+  reducedArr.reduce((acc, cur) => {
+    sumOfReducedArrIndexes += checkSum(arr, acc, cur, arg);
+    return acc;
+  });
+  return sumOfReducedArrIndexes;
 }
 
 function pairwise(arr, arg) {
-  let sum = 0;
-    arr.reduce((acc, curr) => {
-      console.log(acc + " : " + curr);
-      console.log(acc + curr);
-      sum += sumIndexes(arr, acc, curr, arg);
-      return acc;
-    });
-  return sum;
+    let totalSumOfIndexes = 0;
+    let reducedArrGen = reduceFromHead(arr);
+    for(let reducedArr of reducedArrGen)
+        totalSumOfIndexes += checkArrValues(arr, reducedArr, arg);
+    return totalSumOfIndexes;
 }
 
-pairwise([1,4,2,3,0,5], 7);
+console.log(pairwise([0, 0, 0, 0, 1, 1], 1));
